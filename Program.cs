a0d1b5963466f5064d8fbd21c1e6c646d3db3adc
RossyRoo <rossesmusic@gmail.com>
2024-05-09 18:12:07 -0400

    fourth commit

--- Program.cs
+++ Program.cs
@@ -3,11 +3,14 @@
     private static void Main(string[] args)
     {
         string playerName = "";
+        string nextDir;
         
         Console.WriteLine("What is your name?");
         playerName = Console.ReadLine();
 
         Console.WriteLine("Hello " + playerName);
         Console.WriteLine("Where would you like to go?");
+        nextDir = Console.ReadLine();
+        Console.WriteLine("Heading " + nextDir);
     }
 }
