e9920ddaf36f8e001aedc2d31a23790c5b8b2f55
RossyRoo <rossesmusic@gmail.com>
2024-05-09 17:49:46 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string playerName = "";
+        
+        Console.WriteLine("What is your name?");
+        playerName = Console.ReadLine();
+
+        Console.WriteLine("Hello " + playerName);
+    }
+}
